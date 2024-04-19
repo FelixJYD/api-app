cec18e9196703576131d97e1535179b466ef0754
FelixjYD <lukaking9878@gmail.com>
2024-04-19 12:19:45 -0600

    Upgrade laravel 11

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\IncomeController;
+use App\Http\Controllers\UserController;
+use App\Models\Expense;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return response()->json(['Es definitivamente un javascript Object Notation']);
+});
+
+
+Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');
+
+
+
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+//Gastitos
+Route::get('/expenses', [ExpenseController::class, 'index']);
+Route::post('/expenses', [ExpenseController::class, 'store']);
+Route::get('/expenses/{expense}', [ExpenseController::class, 'show']);
+Route::put('/expenses/{expense}', [ExpenseController::class, 'update']);
+Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy']);
+
+//Ingresos
+Route::get('/incomes', [IncomeController::class, 'index']);
+Route::post('/incomes', [IncomeController::class, 'store']);
+Route::get('/incomes/{income}', [IncomeController::class, 'show']);
+Route::put('/incomes/{income}', [IncomeController::class, 'update']);
+Route::delete('/incomes/{income}', [IncomeController::class, 'destroy']);
